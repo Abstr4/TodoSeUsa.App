ba7b4f48828e3b313c26f924acf2c04dd7602401
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-11-03 10:50:01 -0300

    feat: Simple notifications wrapper.

---
+++ TodoSeUsa.BlazorServer/Helpers/SimpleNotifications.cs
@@ -0,0 +1,57 @@
+﻿using Radzen;
+
+namespace TodoSeUsa.BlazorServer.Helpers;
+
+public class SimpleNotifications
+{
+    private readonly NotificationService _notificationService;
+
+    public SimpleNotifications(NotificationService notificationService)
+    {
+        _notificationService = notificationService;
+    }
+
+    public void Success(string detail)
+    {
+        _notificationService.Notify(new NotificationMessage
+        {
+            Severity = NotificationSeverity.Success,
+            Summary = "Operación exitosa",
+            Detail = detail,
+            Duration = 5000
+        });
+    }
+
+    public void Error(string detail)
+    {
+        _notificationService.Notify(new NotificationMessage
+        {
+            Severity = NotificationSeverity.Error,
+            Summary = "Operación fallida",
+            Detail = detail,
+            Duration = 5000
+        });
+    }
+
+    public void Warning(string detail)
+    {
+        _notificationService.Notify(new NotificationMessage
+        {
+            Severity = NotificationSeverity.Warning,
+            Summary = "Cuidado!",
+            Detail = detail,
+            Duration = 5000
+        });
+    }
+
+    public void Info(string detail)
+    {
+        _notificationService.Notify(new NotificationMessage
+        {
+            Severity = NotificationSeverity.Info,
+            Summary = "Info:",
+            Detail = detail,
+            Duration = 5000
+        });
+    }
+}
