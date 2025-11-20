25b4928b98469828dd6061ea52c2a998f6c0f52f
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-11-20 17:35:40 -0300

    consignment model changes

--- TodoSeUsa.Domain/Entities/Consignment.cs
+++ TodoSeUsa.Domain/Entities/Consignment.cs
@@ -4,7 +4,7 @@ public class Consignment : BaseAuditableEntity
 {
     public DateTime DateIssued { get; set; } = DateTime.Now;
 
-    public string Notes { get; set; } = string.Empty;
+    public string? Notes { get; set; } = string.Empty;
 
     public int ProviderId { get; set; }
 
