a711114ab1248d147d39577b9ba9a223f25bff43
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-12-04 17:27:44 -0300

    feat: BaseAuditableEntity UpdatedAt property nullability.

--- TodoSeUsa.Domain/Common/BaseAuditableEntity.cs
+++ TodoSeUsa.Domain/Common/BaseAuditableEntity.cs
@@ -6,7 +6,7 @@ public abstract class BaseAuditableEntity : BaseEntity, ISoftDelete
 {
     public DateTime CreatedAt { get; set; } = DateTime.Now;
 
-    public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdatedAt { get; set; }
 
     public DateTime? DeletedAt { get; set; }
 }
