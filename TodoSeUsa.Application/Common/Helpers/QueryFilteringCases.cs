25c83654513335f64d832f7eea4292098d3e8d8b
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-12-04 17:34:41 -0300

    feat: products filter.

--- TodoSeUsa.Application/Common/Helpers/QueryFilteringCases.cs
+++ TodoSeUsa.Application/Common/Helpers/QueryFilteringCases.cs
@@ -1,20 +1,33 @@
 ﻿using System.Linq.Expressions;
 using TodoSeUsa.Application.Features.Consignments.DTOs;
+using TodoSeUsa.Application.Features.Products.DTOs;
 using TodoSeUsa.Application.Features.Providers.DTOs;
 
 namespace TodoSeUsa.Application.Common.Helpers;
 public static class QueryFilteringCases
 {
-    public static readonly Dictionary<string, Func<string, Expression<Func<Consignment, bool>>>> ConsignmentFilters =
-        new()
+    public static readonly Dictionary<string, Func<string, Expression<Func<Product, bool>>>> ProductFilters =
+    new()
+    {
         {
-            {
-                nameof(ConsignmentDto.ProviderFullName),
-                val => c =>
-                    EF.Functions.Like(c.Provider.Person.FirstName, $"%{val}%") ||
-                    EF.Functions.Like(c.Provider.Person.LastName, $"%{val}%")
-            }
-        };
+            nameof(ProductDto.ProviderInfo),
+            val => c =>
+                EF.Functions.Like(c.Consignment.Provider.Person.FirstName, $"%{val}%") ||
+                EF.Functions.Like(c.Consignment.Provider.Person.LastName, $"%{val}%") ||
+                EF.Functions.Like(c.Consignment.ProviderId.ToString(), $"%{val}%")
+        }
+    };
+
+    public static readonly Dictionary<string, Func<string, Expression<Func<Consignment, bool>>>> ConsignmentFilters =
+    new()
+    {
+        {
+            nameof(ConsignmentDto.ProviderFullName),
+            val => c =>
+                EF.Functions.Like(c.Provider.Person.FirstName, $"%{val}%") ||
+                EF.Functions.Like(c.Provider.Person.LastName, $"%{val}%")
+        }
+    };
 
     public static readonly Dictionary<string, Func<string, Expression<Func<Provider, bool>>>> ProviderFilters =
     new()
