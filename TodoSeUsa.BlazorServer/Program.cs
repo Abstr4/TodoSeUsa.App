f4bdc39575e4cdb54cfe9715988728792a17d9d8
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-12-18 22:09:44 -0300

    feat: Storage folder creation at projects root path.

--- TodoSeUsa.BlazorServer/Program.cs
+++ TodoSeUsa.BlazorServer/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 using TodoSeUsa.Application;
 using TodoSeUsa.BlazorServer;
 using TodoSeUsa.BlazorServer.Components;
@@ -12,28 +13,47 @@ builder.AddApplicationServices();
 builder.AddInfrastructureServices();
 builder.AddWebServices();
 
+var storageRoot = Path.Combine(
+    builder.Environment.ContentRootPath,
+    "Storage",
+    "ProductImages"
+);
+
+Directory.CreateDirectory(storageRoot);
+
+builder.Configuration["Storage:BasePath"] = storageRoot;
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
-    var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<ApplicationDbContext>();
 
     await context.Database.MigrateAsync();
+
     if (app.Environment.IsDevelopment())
     {
-        await DbSeeder.SeedAsync(context);
+        var seeder = services.GetRequiredService<DbSeeder>();
+        await seeder.SeedAsync(context);
     }
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        builder.Configuration["Storage:BasePath"]!
+    ),
+    RequestPath = "/files"
+});
 
 app.UseAntiforgery();
 
