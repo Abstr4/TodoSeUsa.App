ed97a3fb08c412d232d21eed00899e35bbf08e08
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-12-18 21:57:31 -0300

    feat: Product images entity.

---
+++ TodoSeUsa.Domain/Entities/ProductImage.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoSeUsa.Domain.Entities;
+
+public class ProductImage : BaseAuditableEntity
+{
+    public string Path { get; set; } = string.Empty;
+    public bool IsMain { get; set; }
+
+    public int ProductId { get; set; }
+    public Product Product { get; set; } = null!;
+}
