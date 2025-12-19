f1b5a9b43eb634b496b924a7b168670cedc1f505
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2025-12-18 22:05:37 -0300

    feat: ProductImage configuration.

---
+++ TodoSeUsa.Infrastructure/Data/Configurations/ProductImageConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoSeUsa.Infrastructure.Data.Configurations;
+public class ProductImageConfiguration : IEntityTypeConfiguration<ProductImage>
+{
+    public void Configure(EntityTypeBuilder<ProductImage> builder)
+    {
+        builder.UseTpcMappingStrategy();
+
+        builder.ToTable("ProductImages")
+            .HasQueryFilter(b => !b.DeletedAt.HasValue);
+
+        builder.HasKey(i => i.Id);
+
+        builder.HasIndex(i => new { i.ProductId, i.IsMain })
+            .IsUnique()
+            .HasFilter("[IsMain] = 1");
+    }
+}
