230de6092d236edcd2d5ac06d2b5584e6cb1db3c
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2026-01-07 13:43:53 -0300

    feat: scroll to top fab.

---
+++ TodoSeUsa.BlazorServer/wwwroot/lib/js/scrollToTop.js
@@ -0,0 +1,17 @@
+﻿function initScrollToTop() {
+    let mybutton = document.getElementById("myBtn");
+
+    window.addEventListener("scroll", scrollFunction);
+
+    function scrollFunction() {
+        if (window.scrollY > window.innerHeight) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    window.topFunction = function () {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+}
