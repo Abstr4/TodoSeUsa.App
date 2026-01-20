bc4e9d05e228177c2c68e494e9f750d661f199d6
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2026-01-20 14:15:25 -0300

    feat: added simple query request.

---
+++ TodoSeUsa.Application/Common/Models/SimpleQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoSeUsa.Application.Common.Models;
+
+public class SimpleQueryRequest
+{
+    public string? Filter { get; set; }
+    public string? OrderBy { get; set; }
+    public int Skip { get; set; }
+    public int Take { get; set; }
+}
