ab6307efe4ff5665092b91bd7180b03788a6deef
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2026-01-24 01:02:53 -0300

    feat: Code displaying classes for Product and Box.

---
+++ TodoSeUsa.BlazorServer/UI/Styling/DisplayBoxCode.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoSeUsa.BlazorServer.UI.Styling;
+
+internal static class DisplayBoxCode
+{
+    internal static string IdToBoxCode(int boxId)
+    {
+        return $"BOX-{boxId}";
+    }
+}
---
+++ TodoSeUsa.BlazorServer/UI/Styling/DisplayProductCode.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoSeUsa.BlazorServer.UI.Styling;
+
+internal static class DisplayProductCode
+{
+    internal static string IdToProductCode(int productId)
+    {
+        return $"TSU-{productId}";
+    }
+}
