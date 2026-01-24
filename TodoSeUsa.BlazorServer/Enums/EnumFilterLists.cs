3448e19356c421d507acf25ad6be07e553a3911f
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2026-01-24 01:03:38 -0300

    refactor: removed null value for enumerable lists.

--- TodoSeUsa.BlazorServer/Enums/EnumFilterLists.cs
+++ TodoSeUsa.BlazorServer/Enums/EnumFilterLists.cs
@@ -6,45 +6,37 @@ namespace TodoSeUsa.BlazorServer.Enums;
 public static class EnumFilterLists
 {
     public static readonly IEnumerable<Option<ProductQuality?>> qualities =
-    [
-        new Option<ProductQuality?> { Value = null, Text = "Todas" },
-        .. Enum.GetValues<ProductQuality>()
+    [.. Enum.GetValues<ProductQuality>()
             .Cast<ProductQuality>()
             .Select(q => new Option<ProductQuality?>
             {
                 Value = q,
                 Text = EnumTranslate.TranslateQuality(q)
-            }),
+            })
     ];
 
     public static readonly IEnumerable<Option<ProductStatus?>> productStatuses =
-    [
-        new Option<ProductStatus?> { Value = null, Text = "Todos" },
-        .. Enum.GetValues<ProductStatus>()
+    [.. Enum.GetValues<ProductStatus>()
             .Cast<ProductStatus>()
             .Select(s => new Option<ProductStatus?>
             {
                 Value = s,
                 Text = EnumTranslate.TranslateProductStatus(s)
-            }),
+            })
     ];
 
     public static readonly IEnumerable<Option<SaleStatus?>> saleStatuses =
-[
-    new Option<SaleStatus?> { Value = null, Text = "Todos" },
-        .. Enum.GetValues<SaleStatus>()
+    [ .. Enum.GetValues<SaleStatus>()
             .Cast<SaleStatus>()
             .Select(s => new Option<SaleStatus?>
             {
                 Value = s,
                 Text = EnumTranslate.TranslateSaleStatus(s)
-            }),
+            })
     ];
 
     public static readonly IEnumerable<Option<Season?>> seasons =
-    [
-        new Option<Season?> { Value = null, Text = "Todas" },
-        .. Enum.GetValues<Season>()
+    [.. Enum.GetValues<Season>()
             .Cast<Season>()
             .Select(s => new Option<Season?>
             {
@@ -54,9 +46,7 @@ public static class EnumFilterLists
     ];
 
     public static readonly IEnumerable<Option<Body?>> bodies =
-    [
-        new Option<Body?> { Value = null, Text = "Todos" },
-        .. Enum.GetValues<Body>()
+    [ .. Enum.GetValues<Body>()
             .Cast<Body>()
             .Select(b => new Option<Body?>
             {
