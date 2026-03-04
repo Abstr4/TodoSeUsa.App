71fd803aca60790bf39791935ca53edcfcd60b81
Matias Margaritini <matias.rojas@mi.unc.edu.ar>
2026-03-04 16:12:49 -0300

    Migrations

---
+++ TodoSeUsa.Infrastructure/Data/Migrations/20260217175405_ConsignorConstraints.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoSeUsa.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConsignorConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Consignor_CommissionPercentPercent",
+                table: "Consignors",
+                sql: "[CommissionPercent] BETWEEN 0 AND 100");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Consignor_CommissionPercentPercent",
+                table: "Consignors");
+        }
+    }
+}
---
+++ TodoSeUsa.Infrastructure/Data/Migrations/20260217184555_ConsignorCommissionpercent.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TodoSeUsa.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConsignorCommissionpercent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CommissionPercent",
+                table: "Consignors",
+                type: "decimal(3,0)",
+                precision: 3,
+                scale: 0,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,4)",
+                oldPrecision: 18,
+                oldScale: 4);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "CommissionPercent",
+                table: "Consignors",
+                type: "decimal(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(3,0)",
+                oldPrecision: 3,
+                oldScale: 0);
+        }
+    }
+}
